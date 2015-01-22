c6ad85a7a2a99f1518f0d9379ae38b418f4cf592
yangmeansyoung <yangmeansyoung@gmail.com>
2015-01-22 19:43:08 +0800

    add cider plugin

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])  
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[cider/cider-nrepl "0.8.2"]]
+  )  
