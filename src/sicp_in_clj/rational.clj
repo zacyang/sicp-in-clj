e27d659db64567b2288a5c438df1240c5469f1f3
zacyang <yangmeansyoung@gmail.com>
2015-02-25 22:58:01 +0800

    sicp 2.1 practise refactor

--- src/sicp_in_clj/rational.clj
+++ src/sicp_in_clj/rational.clj
@@ -1,19 +1,43 @@
-(ns sicp-in-clj.rational)
+(ns sicp-in-clj.rational
+  "sicp chap 2.1")
 
-;; (defn make-rat [number denom]
-;;   "construct a rational number"
-;;   (list number denom)
-;; )
+(defn- make-rat-stupid-v1 [number denom]
+  "construct a rational number"
+  (list number denom)
+)
 
 ;v2
+(defn- make-rat-v2 [number denom]
+ "construct a rational number"
+ (cond (and (pos? number) (pos? denom)) (make-rat-stupid-v1 number denom)
+       (and (neg? number) (neg? denom)) (make-rat-stupid-v1 (- number) (- denom))
+       :else (make-rat-stupid-v1 (- (Math/abs number)) (Math/abs denom))
+       ))
 
+;v3
 (defn make-rat [number denom]
  "construct a rational number"
- (cond (and (pos? number) (pos? denom)) (list number denom)
-       (and (neg? number) (neg? denom)) (list (- number) (- denom))
-       :else "j"
+ (let [g (gcd number denom)
+       simplified-number (/ number g)
+       simplified-denom (/ denom g)]
+   (make-rat-v2 simplified-number simplified-denom)
+))
+
+;readable v4
+(defn- simplifier [number against]
+  (let [g (gcd number against)]
+    (/ number g)))
+
+(defn- neg-pos-handle [number denom cons-fn]
+  (cond (and (pos? number) (pos? denom)) (cons-fn number denom)
+        (and (neg? number) (neg? denom)) (cons-fn (- number) (- denom))
+        :else (cons-fn (- (Math/abs number)) (Math/abs denom))
        )
 )
+
+(defn readable-make-rat [n d]
+  (neg-pos-handle (simplifier n d) (simplifier d n) make-rat-stupid-v1))
+
 
 (defn number [rational]
   "get the number of a rational number"
@@ -25,3 +49,10 @@
 
 (defn print-rat [rational]
   (println (number rational) "/" (denom rational)))
+
+(defn- gcd "(gcd a b) returns the greatest common divisor of a and b" [a b]
+  (if (or (not (integer? a)) (not (integer? b)))
+    (throw (IllegalArgumentException. "gcd requires two integers"))  
+    (loop [a (Math/abs a) b (Math/abs b)]
+      (if (zero? b) a,
+	  (recur b (mod a b))))))
