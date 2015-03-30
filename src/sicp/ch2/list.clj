2210657cc5dc5388c865d609b9c759085bba737e
zacyang <yangmeansyoung@gmail.com>
2015-03-30 22:11:32 +0800

    [sicp][ch2]2.17 and 2.18 completed

--- src/sicp/ch2/list.clj
+++ src/sicp/ch2/list.clj
@@ -9,3 +9,46 @@
     (first items)
     (list-ref (rest items) (dec n)))
 )
+
+(defn list-length [items]
+  (if (empty? items)
+    0
+    (+ 1 (list-length (rest items)))))
+;;; this is the basic form of recur, if some cond matches ENDs, else continue recur, NOTICE *MUST* recur at the tail
+
+;;; iter approach
+(defn list-length-it [items]
+  (defn len-iter [c count]
+    (if  (empty? c)
+      count
+   (len-iter (rest c) (inc count))))
+
+  (len-iter items 0))
+
+;;; if items-1 is empty, return items-2
+;;; else, append items-2 to (rest items-1) , and cons (first items-1)
+;;; and the sum
+(defn list-append [items-1 items-2]
+  (if (empty? items-1) 
+    items-2
+    (cons (first items-1) (list-append  (rest items-1) items-2)))
+)
+
+;;; 2.17 defn last-pair, this fn should return the (if not nil) col's last element
+(defn last-pair [col]
+  (if (empty? col)
+    (throw (IllegalArgumentException. "SHOULD NOT BE NIL")) 
+    (if (empty? (rest col))
+      (first col)
+      (last-pair (rest col))))) 
+
+;;; 2.18 defn reverse
+(defn col-reverse [col]
+  (if (empty? col)
+    nil
+    (reduce conj '()  col)
+))
+
+
+
+
