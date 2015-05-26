a265dc603a5b546c449ea1436982b97d9ac147e3
zacyang <yangmeansyoung@gmail.com>
2015-05-27 00:55:38 +0800

    [sicp]add book exmaple inorder to complete question

--- src/sicp/ch3/bankacount.clj
+++ src/sicp/ch3/bankacount.clj
@@ -20,3 +20,51 @@
       (deref counter)
       (increase-counter-and-invok-watched-fn watched-fn args)
       )))
+;;; following func is from the book, included inorder to complete the question
+(defn make-account [balance]
+  (def stored-balance (atom balance))
+  
+  )
+
+(defn make-withdraw [balance]
+  (def stored-balance (atom balance))
+  (fn [amount]
+    (if (>= balance amount)
+      (swap! stored-balance #(- % amount))
+      "Insufficient funds"
+      )))
+
+;;; withdraw and deposit all in one
+;; (defn make-account [balance]
+;;   (def stored-balance (atom balance))
+;;   (defmulti bank-opt (fn [opt] opt))
+;;   (defmethod bank-opt :withdraw [_ amount]
+;;     (fn [amount]
+;;       (if (>= balance amount)
+;;         (swap! stored-balance #(- % amount))
+;;         "Insufficient funds"
+;;       )))
+;;   (defmethod bank-opt :deposit [_ amount]
+;;     (fn [amount]
+;;       (swap! stored-balance #(+ % amount))))
+;;   (defmethod bank-opt :default [& _]
+;;     "unsupported opration!")
+;;     )
+
+(defn make-account [balance]
+  (def stored-balance (atom balance))
+  (defn withdraw [amount]
+    (if (>= balance amount)
+      (swap! stored-balance #(- % amount))
+      "Insufficient funds"
+      ))
+  (defn deposit [amount]
+      (swap! stored-balance #(+ % amount)))
+  (defn unsupport [_]
+    "unsupported opration!")
+  
+  (fn [opration] 
+    (cond (= :withdraw opration) withdraw
+          (= :deposit opration) deposit
+          :else (unsupport)
+      )))
