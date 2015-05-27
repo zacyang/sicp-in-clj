fe5070a453f1bf6166d76c2b1894372534601637
zacyang <yangmeansyoung@gmail.com>
2015-05-27 23:32:45 +0800

    [sicp] finished 3.6

--- src/sicp/ch3/getpi.clj
+++ src/sicp/ch3/getpi.clj
@@ -1,34 +1,60 @@
-(ns sicp.ch3.getpi)
+(ns sicp.ch3.getpi
+  (:require [sicp.ch2.rational :as r :only [gcd]]))
 
 ;;; examples on the book
 
 ;;; get pi first version
 
 
-;; (defn rand-update! [x]
-;;   (rand-int 10000)
-;;   )
+(defn rand-update! [x]
+  (rand-int 10000)
+  )
 
-;; (defn rand-num []
-;;   (let [x (atom (rand-int 10000))]
-;;     (swap! x (fn [_] (rand-int 10000)))
-;;     x ))
-
-
-
-;; (defn monte-carlo [trails experiment]
-;;   (defn- iter [trials-remaining trials-passed]
-;;     (cond 
-;;       (zero? trials-remaining) (/ trials-passed trials-remaining)
-;;       (experiment) (iter (dec trials-remaining) (inc trials-passed))
-      ;; :else (iter (dec trials-remaining) trials-passed))))
+(defn rand-num []
+  (let [x (atom (rand-int 10000))]
+    (swap! x (fn [_] (rand-int 10000)))
+    @x ))
 
 
 
-(defn cesaro-test
+(defn monte-carlo [trials experiment]
+  (defn- iter [trials-remaining trials-passed]
+    (cond 
+      (zero? trials-remaining) (/ trials-passed trials)
+      (experiment) (iter (dec trials-remaining) (inc trials-passed))
+      :else (iter (dec trials-remaining) trials-passed)))
+  
+  (iter trials 0)
+)
+
+(defn cesaro-test []
   (= (r/gcd (rand-num) (rand-num))
      1))
 
-
 (defn estimate-pi [trials]
-  (Math/sqrt (/ 6 (mote-carlo trials cesaro-test))))
+  (Math/sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+;;; question remains, solve stack overflow
+(defn rand-num-with [init-value]
+  (let [x (atom init-value)]
+    (swap! x (fn [_] (rand-int 10000)))
+    @x ))
+
+
+(defn rand-num-with-init-parameter 
+
+  ([reset-cmd value]
+   (if (= reset-cmd :reset)
+     (rand-num-with value)
+     :unspported
+     ))
+
+  ([generate-cmd]
+   (if (= generate-cmd :generate)
+     (rand-num-with (rand-int 10000))
+     :unspported
+     )
+  
+  ))
+
+
