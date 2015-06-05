60f83576e66f22dfa7ec37ccce1af16fa370ec85
zacyang <yangmeansyoung@gmail.com>
2015-06-06 01:48:34 +0800

    [sicp]ch3, simulator WIP 2 add more test to cover the frucky funcs

--- src/sicp/ch3/simulator.clj
+++ src/sicp/ch3/simulator.clj
@@ -1,10 +1,13 @@
 (ns sicp.ch3.simulator)
 
+(def ^:private ^:dynamic
+  *the-agenda* (atom '()))
+
 (defn make-wire
   "make wire , a presentation of the logical wires"
   []
   (let [signal-value (atom 0)
-        action-procedures '()]
+        action-procedures (atom '())]
 
     (defn- set-my-singnal! 
       [new-value]
@@ -15,9 +18,8 @@
     
     (defn- accept-action-procedure!
       [proc]
-      (swap! action-procedures (fn [x] (cons proc action-procedures)))
-      (proc)
-      )
+      (swap! action-procedures (fn [x] (cons proc x)))
+      (proc))
 
     (defn- get-signal-value
       []
@@ -35,12 +37,12 @@
   (wire :get-signal))
 
 (defn set-signal!
-  [wire]
-  (wire :set-signal!))
+  [wire signal]
+  (wire :set-signal!) signal)
 
 (defn add-action!
-  [wire]
-  (wire :add-action!))
+  [wire action]
+  ((wire :add-action!) action))
 
 (defn logical-not 
   [x]
@@ -50,54 +52,174 @@
 
 (defn call-each
   [procs]
-  (if (or (empty? procs) (nil? procs))
+  (if  (empty? procs) 
     :done
     (do
-      ((first (doall procs)))
+      ((first  procs))
       (call-each (rest procs))
-      )
-    
-))
-(defn after-delay [delay proc]
+      )))
+
+
+(defn after-delay 
+  [delay action]
+  (add-to-agenda! (+ delay (current-time *the-agenda*)) 
+                  action
+                  *the-agenda*))
+
+(def inverter-delay 1)
+
+(defn inverter 
+  [input output]
+  (defn- invert-input
+    []
+    (let [new-value (logical-not (get-signal input))]
+      (after-delay inverter-delay
+                   (#(set-signal! output new-value)))))
+  (add-action! input invert-input)
+  :OK
 )
-;; (defn inverter 
-;;   [input output]
-;;   (defn- invert-input
-;;     []
-;;     (let [new-value (logical-not (get-signal input))]
-;;       (after-delay inverter-delay
-;;                    #(set-signal! output new-value))))
 
-;;   (add-action! input invert-input)
-;; )
+(defn logical-and 
+  [signal-1 signal-2]
+  (if (and (= signal-1 1) (= signal-2 1)) 
+    1
+    0))
 
+(def and-gate-delay 1)
 
-
-
-
-
-;; (defn and-gate
-;;   "logical or, for simulate or-gate behavious"
-;;   [input-1 input-2 output]
+(defn and-gate
+  "logical or, for simulate or-gate behavious"
+  [input-1 input-2 output]
   
-;;   (defn and-action-procedure
-;;     []
-;;     (let [new-value (logical-and (get-signal input-1) (get-signal input-2))]
-;;       (after-delay and-get-delay
-;;                    (#(set-signal! out-put new-value)))
+  (defn and-action-procedure
+    []
+    (let [new-value (logical-and (get-signal input-1) (get-signal input-2))]
+      (after-delay and-gate-delay
+                   #(set-signal! output new-value)))))
 
-;; ))
+;;; practise 3.28
+(defn logical-or
+  [signal-1 signal-2]
+  (if (or (= signal-1 1) (= signal-2 1))
+    1
+    0))
+
+(def or-gate-delay 1)
+
+(defn or-gate
+  [input-1 input-2 output]
+  (defn or-gate-procedure
+    []
+    (let [new-value (logical-or (get-signal input-1) (get-signal input-2))]
+      (after-delay and-gate-delay
+                   #(set-signal! output new-value)))))
+
+;;; 3.29
+(defn or-gate-with-combination 
+  [input-1 input-2 output]
+  (let [w1 (make-wire)
+        w2 (make-wire)
+        w3 (make-wire)]
+    (inverter input-1 w1)
+    (inverter input-2 w2)
+    (and-gate w1 w2 w3)
+    (inverter w3 output)))
+
+
+
+(defn make-time-segment 
+  [time queue]
+  (list time queue))
+
+(defn segment-time 
+  [segment]
+  (first segment))
+
+(defn segment-queue
+  [segment]
+  (last segment))
+
+;;; define agenda
+(defn make-agenda
+  []
+  (atom (list 0)))
+
+(defn current-time
+  [agenda]
+  (first agenda))
+
+
+;;; problem !!!!!!!!!!!!!
+(defn set-current-time! 
+  [agenda time]
+  (swap! agenda (fn [x] (list (atom time)))))
+
+(defn segments 
+  [agenda]
+  (last agenda))
+
+(defn first-segment
+  [agenda]
+  (first (segments agenda)))
+
+(defn rest-segment
+  [agenda]
+  (rest (segments agenda)))
+
+(defn empty-agenda?
+  [agenda]
+  (empty? (segments agenda)))
+
+;; (defn add-to-agenda! 
+;;   [time action agenda]
+;;   (defn- belongs-before?
+;;     [segments]
+;;     (or (empty? segments) (< time (segment-time (first segments)))))
+;;   (defn- make-new-time-segment
+;;     [time action]
+;;     (let [q (make-queue)]
+;;       (insert-queue! q action)
+;;       (make-time-segment time q)))
+;;   (defn add-to-segements! 
+;;     [segments]
+;;     ())
+
+
 ;; )
+
 
 ;;; code on 3.3.4 just convert from scheme to clojure LOL
 
-(def a (make-wire))
-(def b (make-wire))
-(def c (make-wire))
+;; (def a (make-wire))
+;; (def b (make-wire))
+;; (def c (make-wire))
 
-(def d (make-wire))
-(def e (make-wire))
-(def s (make-wire))
+;; (def d (make-wire))
+;; (def e (make-wire))
+;; (def s (make-wire))
 
+(defn first-agenda-item
+  [agenda])
 
+(defn remove-first-agenda-item!
+  [agenda])
+
+;;; propagate
+(defn propagate
+  []
+  (if (empty-agenda? *the-agenda*)
+    :done
+    (let [first-item (first-agenda-item *the-agenda*)]
+      (first-item)
+      (remove-first-agenda-item! *the-agenda*)
+      (propagate))))
+
+(defn probe
+  [name wire]
+  (add-action! wire
+               #(
+                 (println "----------")
+                 (println name)
+                 (println (current-time *the-agenda*))
+                 (println "new-value = " (get-signal wire)))))
 
--- test/sicp/ch3/simulator_test.clj
+++ test/sicp/ch3/simulator_test.clj
@@ -2,6 +2,12 @@
   (:use [midje.sweet]
         [sicp.ch3.simulator]))
 
+(defn- dummy-fn-just-return 
+  [] 
+  (fn [] 
+    (swap! counter #(inc %))
+          @counter
+          ))
 
 (fact "should be able to make a wire"
       (let [fn-under-test (make-wire)]
@@ -9,20 +15,16 @@
         ((fn-under-test :set-signal!) 1) => 1
         ;;then the signal should be stored as 1
         ((fn-under-test :get-signal)) => 1
-        
+        ((fn-under-test :add-action!) (fn [] 2)) => 1
           ))
 
 (fact "call-each test"
       (def  counter (atom 0))
-      (defn dummy-fn-just-return 
-        [] 
-        (fn [] 
-          (swap! counter #(inc %))
-          @counter
-          ))
+
       
       (let [fn-under-test call-each
             procs  (take 3 (repeat (dummy-fn-just-return)))]
         (fn-under-test procs) => :done
         @counter => 3
         ))
+
